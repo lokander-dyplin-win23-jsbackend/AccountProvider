f2d06857370acaf5ef04ae71e49c12ac96f9e103
Markus Lokander <Markuslokander97@gmail.com>
2024-05-09 10:13:26 +0200

    added sql connection string

---
+++ Data/Contexts/DataContextFactory.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace Data.Contexts;
+
+internal class DataContextFactory : IDesignTimeDbContextFactory<DataContext>
+{
+    public DataContext CreateDbContext(string[] args)
+    {
+        var optionsBuilder = new DbContextOptionsBuilder<DataContext>();
+        optionsBuilder.UseSqlServer("Server=tcp:sqlserver-lokadypl.database.windows.net,1433;Initial Catalog=accounts;Persist Security Info=False;User ID=SqlAdmin;Password=Bytmig1234!;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
+
+        return new DataContext(optionsBuilder.Options);
+    }
+}
