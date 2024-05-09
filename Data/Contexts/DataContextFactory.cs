17acf2f49009a56f49e2f633a055c30f11babf8d
Markus Lokander <Markuslokander97@gmail.com>
2024-05-09 11:24:11 +0200

    added connection to Azure and updated DB

--- Data/Contexts/DataContextFactory.cs
+++ Data/Contexts/DataContextFactory.cs
@@ -8,7 +8,7 @@ internal class DataContextFactory : IDesignTimeDbContextFactory<DataContext>
     public DataContext CreateDbContext(string[] args)
     {
         var optionsBuilder = new DbContextOptionsBuilder<DataContext>();
-        optionsBuilder.UseSqlServer("Server=tcp:sqlserver-lokadypl.database.windows.net,1433;Initial Catalog=accounts;Persist Security Info=False;User ID=SqlAdmin;Password=Bytmig1234!;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
+        optionsBuilder.UseSqlServer("Server=tcp:sqlserver-lokadypl.database.windows.net,1433;Initial Catalog=accounts;Persist Security Info=False;User ID=SqlAdmin;Password=Bytmig1234;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
 
         return new DataContext(optionsBuilder.Options);
     }
