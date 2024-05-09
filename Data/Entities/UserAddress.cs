e8e2fcaa5388b4211890b26f09e027dea8c09378
Markus Lokander <Markuslokander97@gmail.com>
2024-05-09 10:06:40 +0200

    added UserAccount/UserAddress

---
+++ Data/Entities/UserAddress.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Data.Entities;
+
+public class UserAddress
+{
+    [Key]
+    public string Id { get; set; } = Guid.NewGuid().ToString();
+    public string AddressType { get; set; } = null!;
+    public string AddressLine_1 { get; set; } = null!;
+    public string? AddressLine_2 { get; set; }
+    public string PostalCode { get; set; } = null!;
+    public string City { get; set; } = null!;
+}
