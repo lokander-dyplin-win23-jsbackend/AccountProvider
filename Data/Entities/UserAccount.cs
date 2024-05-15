f345315b108f617dc02a5a9cf69873221cf8e3a7
Markus Lokander <Markuslokander97@gmail.com>
2024-05-16 00:02:36 +0200

    removed profileimage

--- Data/Entities/UserAccount.cs
+++ Data/Entities/UserAccount.cs
@@ -6,8 +6,6 @@ public class UserAccount : IdentityUser
 {
     public string? FirstName { get; set; }
     public string? LastName { get; set;}
-    public string ProfileImage { get; set; } = null!;
-
     public string? AddressId { get; set; }
 
     public UserAddress? Address { get; set; }
---
+++ Data/Migrations/20240515220148_removed profileimage.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class removedprofileimage : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ProfileImage",
+                table: "AspNetUsers");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ProfileImage",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
