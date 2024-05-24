500ad8cf18836396f565fc93a989f8131ba2fd8d
Markus Lokander <Markuslokander97@gmail.com>
2024-05-24 11:20:12 +0200

    Json reading fix

--- AccountProvider/Functions/SignIn.cs
+++ AccountProvider/Functions/SignIn.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
+using System.Net.Http.Json;
 using System.Text;
 
 namespace AccountProvider.Functions
@@ -51,27 +52,34 @@ namespace AccountProvider.Functions
                         {
                             if (userAccount!.Email != null && userAccount.Id != null)
                             {
-                                TokenRequest trm = new TokenRequest
+
+
+
+                                TokenRequest tr = new TokenRequest
                                 {
                                     Email = userAccount.Email!,
                                     UserId = userAccount.Id,
                                 };
 
-                                try
-                                {
-                                    trm = JsonConvert.DeserializeObject<TokenRequest>(body)!;
-                                }
-
-                                catch (Exception ex)
-                                {
-                                    _logger.LogError($"JsonConvert TRM :: {ex.Message}");
-                                }
-
-
                                 using var http = new HttpClient();
-                                StringContent content = new StringContent(JsonConvert.SerializeObject(trm), Encoding.UTF8, "application/json");
-                                var response = await http.PostAsync("https://tokenprovider-silicons.azurewebsites.net/api/token/generate?code=wuBVxONg8p6LBOAPV_MQ8E2Q4YpHZ1yZqBai51eSCNEAAzFut47-Uw%3D%3D", content);
-                                return new OkObjectResult("accessToken");
+                                var response = await http.PostAsJsonAsync("https://tokenprovider-silicons.azurewebsites.net/api/token/generate?code=wuBVxONg8p6LBOAPV_MQ8E2Q4YpHZ1yZqBai51eSCNEAAzFut47-Uw%3D%3D", tr);
+                                var tokenResponseContent = await response.Content.ReadAsStringAsync();
+
+                                // Assuming the token provider returns a JSON object with the access token
+                                // Example: { "accessToken": "your_access_token_here" }
+                                var tokenResponse = JsonConvert.DeserializeObject<Dictionary<string, string>>(tokenResponseContent);
+
+                                if (tokenResponse != null && tokenResponse.ContainsKey("accessToken"))
+                                {
+                                    return new OkObjectResult(new { token = tokenResponse["accessToken"] });
+                                }
+                                else
+                                {
+                                    return new BadRequestObjectResult(new { error = "Token generation failed" });
+                                }
+
+
+
                             }
 
                             return new UnauthorizedResult();
